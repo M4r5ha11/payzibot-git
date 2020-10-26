60ced8c4cc6aea282e8e19536e4480c2be0784c6
KatrineD3V <57178942+KatrineD3V@users.noreply.github.com>
2020-10-27 01:22:17 +0200

    Update fox.js

--- payzibot/cmds/fox.js
+++ payzibot/cmds/fox.js
@@ -1,15 +1,13 @@
 exports.run = (client, message, args) => {
-        const Discord = require('discord.js')
-        require('axios')('https://randomfox.ca/floof/?ref=apilist.fun').then(async res => {
-      message.channel.send(
-         new Discord.MessageEmbed().setColor('#064f3b').setTitle('**Рандомный лис**').setAuthor(message.author.tag, message.author.displayAvatarURL(), message.author.url).setImage(res.data.image)
-      )
+    const { MessageEmbed } = require('discord.js')
+    require('axios')('https://randomfox.ca/floof/?ref=apilist.fun').then(async res => {
+        message.channel.send(new MessageEmbed().setColor('#064f3b').setTitle('**Рандомный лис**').setAuthor(message.author.tag, message.author.avatarURL(), message.author.url).setImage(res.data.image))
     })
-        }
-       exports.help = {
+}
+exports.help = {
     name: "fox",
     aliases: [],
     info: "photo",
     usage: "fox",
     description: "Фото лисы/лиса"
-    }
+}
