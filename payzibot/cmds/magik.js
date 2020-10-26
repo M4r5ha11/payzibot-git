73ba8955dc825fd05b403a71e1b484dabdcc866f
KatrineD3V <57178942+KatrineD3V@users.noreply.github.com>
2020-10-27 01:50:39 +0200

    Update magik.js

--- payzibot/cmds/magik.js
+++ payzibot/cmds/magik.js
@@ -1,21 +1,18 @@
- exports.run = async (client, message, args) => {
-        const Discord = require('discord.js')
-         let member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]))
-            let argsUser
-    if (member) argsUser = member.user
-    else argsUser = message.author
-   let ava = argsUser.displayAvatarURL({format: 'png', size: 1024})
-  let embed = new Discord.MessageEmbed()
+exports.run = async (client, message, args) => {
+    const { MessageEmbed } = require('discord.js')
+    let member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]))
+    let argsUser = member.user || message.author
+    let ava = argsUser.avatarURL({format: 'png', size: 1024})
+    message.channel.send(new MessageEmbed()
         .setColor('#064f3b')
         .setTitle('**Magik-avatar**')
-        .setAuthor(message.author.tag, message.author.displayAvatarURL(), message.author.url)
-        .setImage(await require('node-fetch')(`https://nekobot.xyz/api/imagegen?type=magik&image=${ava}&intensity=1`).then(r => r.json()).then(r => r.message))
-      message.channel.send(embed)
-        }
-       exports.help = {
+        .setAuthor(message.author.tag, message.author.avatarURL(), message.author.url)
+        .setImage(await require('node-fetch')(`https://nekobot.xyz/api/imagegen?type=magik&image=${ava}&intensity=1`).then(r => r.json()).then(r => r.message)))
+}
+exports.help = {
     name: "magik",
     aliases: ['magic'],
     info: "photo",
     usage: "magik (пользователь)",
     description: "Помять аватарку упомянутого пользователя"
-    }
+}
