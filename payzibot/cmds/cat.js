1e608a12e4ca8c16928c01ef194c463e287b0bb6
KatrineD3V <57178942+KatrineD3V@users.noreply.github.com>
2020-10-27 01:05:40 +0200

    Update cat.js

--- payzibot/cmds/cat.js
+++ payzibot/cmds/cat.js
@@ -1,15 +1,13 @@
 exports.run = (client, message, args) => {
-        const Discord = require('discord.js')
-                require('axios')('http://aws.random.cat/meow').then(async res => {
-       message.channel.send(
-         new Discord.MessageEmbed().setColor('#064f3b').setTitle('**Рандомный кот**').setAuthor(message.author.tag, message.author.displayAvatarURL(), message.author.url).setImage(res.data.file)
-      )
+    const { MessageEmbed } = require('discord.js')
+    require('axios')('http://aws.random.cat/meow').then(async res => {
+        message.channel.send(new MessageEmbed().setColor('#064f3b').setTitle('**Рандомный кот**').setAuthor(message.author.tag, message.author.avatarURL(), message.author.url).setImage(res.data.file))
     })
-        }
-       exports.help = {
-    name: "catупкрекпкр5е",
+}
+exports.help = {
+    name: "cat",
     aliases: [],
     info: "photаo",
     usage: "cat",
     description: "Фото кота/кошки"
-    }
+}
