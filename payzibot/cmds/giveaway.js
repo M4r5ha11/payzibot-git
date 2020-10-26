1ab730bc4e05b3626e0d268a654076f054e487c9
KatrineD3V <57178942+KatrineD3V@users.noreply.github.com>
2020-10-27 01:36:27 +0200

    Update giveaway.js

--- payzibot/cmds/giveaway.js
+++ payzibot/cmds/giveaway.js
@@ -1,52 +1,48 @@
 const Discord = require('discord.js')
 exports.run = async (client, message, args) => {
-  var item = "";
-var time;
-var winnerCount
-let messageArray = message.content.split(" ");
-
-for (var i = 3; i < messageArray.length; i++){
-  item += (messageArray[i] + " ");
-}
-winnerCount = Number(messageArray[1]);
-time = Number(messageArray[2]);
-
-
-var giveEmbed = new Discord.MessageEmbed();
-giveEmbed.setTitle("🎉 Розыгрыш 🎉");
-giveEmbed.setDescription(`\n**Приз: ${item}** \n\n**Создал розыгрыш: ${message.author}**`)
-let embedSent = await message.channel.send(giveEmbed);
-let react = await embedSent.react("🎉");
-setTimeout(async function() {
-var reactFetch = await react.fetch("🎉");
-var peopleReacted = reactFetch.users.cache.array().filter(us => !us.bot);
-  var winners = [];
-
-  if (peopleReacted.length >= winnerCount) {
-    winners = peopleReacted;
-  } else {
-    for (var i = 0; i < winnerCount; i++){
-      var index = Math.floor(Math.random() * peopleReacted.length);
-      winners.push(peopleReacted[index]);
-      peopleReacted.splice(index, 1);
+    let item = "",
+        time,
+        winnerCount,
+        messageArray = message.content.split(" ");
+    for (var i = 3; i < messageArray.length; i++){
+        item += (messageArray[i] + " ");
     }
-  }
-  var winnerMsg = "Участник(и) ";
-  for (var i = 0; i < winners.length; i++){
-       if(!winners[i]) continue
-    winnerMsg += ('<' + '@' + winners[i] + '>' + " ");
-  }
-  var Embed = new Discord.MessageEmbed();
-  Embed.setTitle("🎉 Розыгрыш завершён! 🎉")
-  Embed.setColor('RANDOM')
-  Embed.setDescription(`**${winnerMsg} выиграл(и) ${item}**`)
-  message.channel.send(Embed); //мне нужно главное сообщение, едитировать на Embed
-}, time * 1000);
+    winnerCount = Number(messageArray[1]);
+    time = Number(messageArray[2]);
+    var giveEmbed = new Discord.MessageEmbed()
+        .setTitle("🎉 Розыгрыш 🎉")
+        .setDescription(`\n**Приз: ${item}** \n\n**Создал розыгрыш: ${message.author}**`);
+    let embedSent = await message.channel.send(giveEmbed);
+    let react = await embedSent.react("🎉");
+    setTimeout(async function() {
+        var reactFetch = await react.fetch("🎉"),
+            peopleReacted = reactFetch.users.cache.array().filter(us => !us.bot),
+            winners = [];
+        if (peopleReacted.length >= winnerCount) {
+            winners = peopleReacted;
+        } else {
+            for (var i = 0; i < winnerCount; i++) {
+                var index = Math.floor(Math.random() * peopleReacted.length);
+                winners.push(peopleReacted[index]);
+                peopleReacted.splice(index, 1);
+            }
+        }
+        var winnerMsg = "Участник(и) ";
+        for (var i = 0; i < winners.length; i++){
+            if(!winners[i]) continue
+            winnerMsg += ('<' + '@' + winners[i] + '>' + " ");
+       }
+       var Embed = new Discord.MessageEmbed()
+           .setTitle("🎉 Розыгрыш завершён! 🎉")
+           .setColor('RANDOM')
+           .setDescription(`**${winnerMsg} выиграл(и) ${item}**`);
+      message.channel.send(Embed); //мне нужно главное сообщение, едитировать на Embed
+    }, time * 1000);
 }
-       exports.help = {
-    name: "giveawреерекпay",
+exports.help = {
+    name: "giveaway",
     aliases: [],
     info: "g",
     usage: "giveaway () () ()",
     description: "Провести розыгрыш"
-    }
+}
