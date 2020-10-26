ba355d606be4824e3bd7673dac6951e6ecfaecf4
KatrineD3V <57178942+KatrineD3V@users.noreply.github.com>
2020-10-27 01:47:31 +0200

    Update link.js

--- payzibot/cmds/link.js
+++ payzibot/cmds/link.js
@@ -1,15 +1,14 @@
 exports.run = (client, message, args) => {
-        const Discord = require('discord.js')
-        let  embed1 = new Discord.MessageEmbed()
-        .addField ('Важные ссылки', `[Пригласить бота](https://discord.com/api/oauth2/authorize?client_id=576442351426207744&permissions=8&scope=bot)\n[Саппорт сервер](https://discord.gg/XuN4uhC)`)
-        .addField ('Мониторинги', `[SD.C](https://bots.server-discord.com/576442351426207744)\n[Top.gg](https://top.gg/bot/576442351426207744)`)
-        .setColor('#064f3b')
-        message.channel.send(embed1)
-       }
-       exports.help = {
+    const { MessageEmbed } = require('discord.js')
+    message.channel.send(new MessageEmbed()
+        .addField('Важные ссылки', `[Пригласить бота](https://discord.com/api/oauth2/authorize?client_id=576442351426207744&permissions=8&scope=bot)\n[Саппорт сервер](https://discord.gg/XuN4uhC)`)
+        .addField('Мониторинги', `[SD.C](https://bots.server-discord.com/576442351426207744)\n[top.gg](https://top.gg/bot/576442351426207744)`)
+        .setColor('#064f3b'))
+}
+exports.help = {
     name: "link",
     aliases: ['invite'],
     info: "general",
     usage: "link",
     description: "Ссылки бота"
-    }
+}
