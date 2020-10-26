8b3b5a89527e8c7eda7525cb0beefb3f0b795fa5
KatrineD3V <57178942+KatrineD3V@users.noreply.github.com>
2020-10-27 01:39:24 +0200

    Update hentai.js

--- payzibot/cmds/hentai.js
+++ payzibot/cmds/hentai.js
@@ -1,18 +1,17 @@
 exports.run = async (client, message, args) => {
-        const Discord = require('discord.js')
-        if (!message.channel.nsfw) return message.reply('Данный канал не NSFW!')
-  let embed = new Discord.MessageEmbed()
+    const Discord = require('discord.js')
+    if (!message.channel.nsfw) return message.reply('Данный канал не NSFW!')
+    let embed = new Discord.MessageEmbed()
         .setColor('#064f3b')
         .setTitle('**Рандомный хентай**')
-        .setAuthor(message.author.tag, message.author.displayAvatarURL(), message.author.url)
+        .setAuthor(message.author.tag, message.author.avatarURL(), message.author.url)
         .setImage(await require('node-fetch')(`https://nekobot.xyz/api/image?type=hentai`).then(r => r.json()).then(r => r.message))
-      message.channel.send(embed)
-      
-        }
-       exports.help = {
+    message.channel.send(embed)
+}
+exports.help = {
     name: "hentai",
     aliases: [],
     info: "nsfw",
     usage: "hentai",
     description: "Рандомный хентай"
-    }
+}
