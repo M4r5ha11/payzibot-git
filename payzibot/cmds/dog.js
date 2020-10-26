6e78667fa88b80fc882a63f28dde3eed1fad2e28
KatrineD3V <57178942+KatrineD3V@users.noreply.github.com>
2020-10-27 01:17:07 +0200

    Update dog.js

--- payzibot/cmds/dog.js
+++ payzibot/cmds/dog.js
@@ -1,15 +1,13 @@
 exports.run = (client, message, args) => {
-        const Discord = require('discord.js')
-        require('axios')('https://dog.ceo/api/breeds/image/random').then(async res => {
-      message.channel.send(
-         new Discord.MessageEmbed().setColor('#064f3b').setTitle('**Рандомный пёс**').setAuthor(message.author.tag, message.author.displayAvatarURL(), message.author.url).setImage(res.data.message)
-      )
+    const { MessageEmbed } = require('discord.js')
+    require('axios')('https://dog.ceo/api/breeds/image/random').then(async res => {
+        message.channel.send(new MessageEmbed().setColor('#064f3b').setTitle('**Рандомный пёс**').setAuthor(message.author.tag, message.author.avatarURL(), message.author.url).setImage(res.data.message))
     })
-        }
-       exports.help = {
+}
+exports.help = {
     name: "dog",
     aliases: [],
     info: "photo",
     usage: "dog",
     description: "Фото собаки"
-    }
+}
