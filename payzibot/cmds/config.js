ec5fc1fac7365135ff6c4e84a9da740c9db323a2
KatrineD3V <57178942+KatrineD3V@users.noreply.github.com>
2020-10-27 01:13:09 +0200

    Update config.js

--- payzibot/cmds/config.js
+++ payzibot/cmds/config.js
@@ -1,27 +1,23 @@
 exports.run = (client, message, args) => {
-        const Discord = require('discord.js')
-          const Guild = require('../db/guild.js')
-            Guild.findOne({guildID: message.guild.id}, (err,data) => {
-        if(err) return client.channels.cache.get("716291186645794878").send(`:x: | Произошла ошибка при поиске сервера в базе-данных`) 
-       
-       if(!data){
-           message.reply('Сервер отсутствует в базе данных!')
-       }else{
-       let premium;
-       if(data.premium === true)premium = `Активен`
-       if(data.premium === false)premium = `Неактивен`
-       let embed = new Discord.MessageEmbed()
-        .setTitle('Конфиг бота на сервере server'.replace('server', message.guild.name))
-        .addField ('Главное', 'Префикс - `pref`\nВалюта - val\nПремиум - premium'.replace('pref', data.prefix).replace('val', data.currency).replace('premium', premium))
-        message.channel.send(embed)
+    const { MessageEmbed } = require('discord.js'),
+          Guild = require('../db/guild.js');
+    Guild.findOne({ guildID: message.guild.id }, (err, data) => {
+        if (err) return client.channels.cache.get("716291186645794878").send(`:x: | Произошла ошибка при поиске сервера в базе-данных`);
+        if (!data) message.reply('Сервер отсутствует в базе данных!')
+        else {
+           /*let premium;
+           if (data.premium === true) premium = `Активен`
+           if (data.premium === false) premium = `Неактивен`*/
+           message.channel.send(new MessageEmbed()
+          .setTitle('Конфиг бота на сервере server'.replace('server', message.guild.name))
+          .addField('Главное', 'Префикс - `pref`\nВалюта - val\nПремиум - premium'.replace('pref', data.prefix).replace('val', data.currency).replace('premium', data.premium ? "Активен" : "Неактивен")))
+        }
+    })
 }
-        })
-
-       }
-       exports.help = {
-    name: "hthttconfig",
-    aliases: ['hthyjyujconf'],
+exports.help = {
+    name: "config",
+    aliases: ['conf'],
     info: "settings",
     usage: "config",
     description: "Посмотреть настройки бота для данного сервера"
-    }
+}
